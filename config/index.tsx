82ad2f5f3b8b62319467e57946c2ec333019051f
boidepiranha <boidepiranha@gmail.com>
2025-03-22 13:47:18 -0300

    Corrigido problema com projectId para Vercel

--- config/index.tsx
+++ config/index.tsx
@@ -1,12 +1,11 @@
-
 // config/index.tsx
 
 import { cookieStorage, createStorage, http } from '@wagmi/core'
 import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
 import { mainnet, arbitrum } from '@reown/appkit/networks'
 
-// Get projectId from https://cloud.reown.com
-export const projectId = '8aca3b20849e7f571c1d44808e12aae6'
+// Get projectId from environment variable, fallback to hardcoded value
+export const projectId = process.env.NEXT_PUBLIC_WC_PROJECT_ID || '8aca3b20849e7f571c1d44808e12aae6'
 
 if (!projectId) {
   throw new Error('Project ID is not defined')
